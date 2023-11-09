ea20800df2e1a1196b5017a645b447a910957af9
BuildSystems <131750087+BuildsystemsX@users.noreply.github.com>
2023-11-09 18:14:11 +0100

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,16 @@ import 'styles/globals.css'
 import { PlasmicRootProvider } from "@plasmicapp/react-web";
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import * as React from 'react';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <PlasmicRootProvider Head={Head}>
-      <Component {...pageProps} />
-    </PlasmicRootProvider>
-  );
+    // @ts-ignore
+    return <PlasmicRootProvider Head={Head} skipFonts>
+        <Head>
+            <link rel="icon" href="/black-logo.svg" sizes="any"/>
+            <link rel="icon" href="/black-logo.png" sizes="any"/>
+            <link rel="apple-touch-icon" href="/black-logo.png"/>
+        </Head>
+        <Component {...pageProps} />
+    </PlasmicRootProvider>;
 }
